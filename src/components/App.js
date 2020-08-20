e2578cbe9987d9953a1fec4b059c0b41da21d50c
niallgreene7 <niall251@googlemail.com>
2020-08-20 17:38:58 +0100

    created project

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Surreal Estate</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
